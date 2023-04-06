da7c69b4afdd5895b470899aa5743ed429299911
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-04-06 21:55:23 +0300

    Saving output as json in a json file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { request, gql } from 'graphql-request'
+import fs from 'fs'
 import users from './users.json'
 
 interface User {
@@ -8,12 +9,28 @@ interface User {
 const endpoint =
   'https://v4.subgraph.mainnet.oceanprotocol.com/subgraphs/name/oceanprotocol/ocean-subgraph'
 
+function saveOutput(jsonData: any) {
+  const jsonString = JSON.stringify(jsonData, null, 2)
+
+  fs.writeFile('output.json', jsonString, (err) => {
+    if (err) {
+      console.error('Error writing JSON data to file:', err)
+    } else {
+      console.log('JSON data successfully written to file')
+    }
+  })
+}
 export default async function run() {
   const userArray: User[] = users as User[]
+  const output: any = []
 
   for (const user of userArray) {
     const query = gql`
       query {
+        nfts(where:{creator: "${user.address}"}){
+          id,
+          name
+        },
         veOCEAN(id: "${user.address}") {
           id
           lockedAmount,
@@ -23,14 +40,15 @@ export default async function run() {
         }
       }
     `
-
     try {
       const data: any = await request(endpoint, query)
-      console.log(data.veOCEAN.lockedAmount)
+      console.log(data.nfts[0]?.name, data.veOCEAN?.lockedAmount)
+      output.push(data)
     } catch (err) {
       console.error(err)
     }
   }
+  saveOutput(output)
 }
 
 run()
