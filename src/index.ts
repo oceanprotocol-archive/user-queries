c7364ca7c9293b63b5a07c3331fa50a94a2d045b
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-04-11 18:25:29 +0300

    Adding import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { request, gql } from 'graphql-request'
-import users from './users.json'
+import users from './holder_summary_onchain - holder_summary_onchain.json'
 import { getBlockNumberXDaysAgo, saveOutput } from './utils'
 import { User, Output } from './types'
 
@@ -51,6 +51,20 @@ export default async function run() {
         if (!output[i]) {
           output[i] = {
             userAddress: userArray[i].address,
+            first_tx_block: userArray[i].first_tx_block,
+            first_tx_ts: userArray[i].first_tx_ts,
+            first_tx_date: userArray[i].first_tx_date,
+            first_tx_id: userArray[i].first_tx_id,
+            last_tx_block: userArray[i].last_tx_block,
+            last_tx_ts: userArray[i].last_tx_ts,
+            last_tx_date: userArray[i].last_tx_date,
+            last_tx_id: userArray[i].last_tx_id,
+            txs_3mo: userArray[i].txs_3mo,
+            txs_6mo: userArray[i].txs_6mo,
+            txs_12mo: userArray[i].txs_12mo,
+            txs_24mo: userArray[i].txs_24mo,
+            txs_48mo: userArray[i].txs_48mo,
+            bal_OCEAN: userArray[i].bal_OCEAN,
             data: []
           }
         }
